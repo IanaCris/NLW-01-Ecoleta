11ff9c96b44cf3f933200221c8b9ce15f778f86c
IanaCris <ianasousati@gmail.com>
2020-06-17 01:54:09 -0300

    upload images multer

--- server/src/controllers/PointsController.ts
+++ server/src/controllers/PointsController.ts
@@ -22,7 +22,14 @@ class PointsController {
       return response.status(400).json({ message: 'No collection points! '});
     }
 
-    return response.json(points);
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.100.7:3333/uploads/${point.image}`,
+      };
+    });
+
+    return response.json(serializedPoints);
 
   }
 
@@ -35,12 +42,17 @@ class PointsController {
       return response.status(400).json({ message: 'Point not found! '});
     }
 
+    const serializedPoint = {
+        ...point,
+        image_url: `http://192.168.100.7:3333/uploads/${point.image}`,
+    };
+
     const items = await knex('items')
       .join('point_items', 'items.id', '=', 'point_items.item_id')
       .where('point_items.point_id', id)
       .select('items.title');
 
-    return response.json({ point, items });
+    return response.json({ point: serializedPoint, items });
   }  
 
   async create(request: Request, response: Response)  {
@@ -58,7 +70,7 @@ class PointsController {
     const trx = await knex.transaction();
 
     const point = {
-      image: 'https://images.unsplash.com/photo-1583258292688-d0213dc5a3a8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+      image: request.file.filename,
       name,
       email,
       whatsapp,
@@ -72,11 +84,14 @@ class PointsController {
   
     const point_id = insertedIds[0];
   
-    const pointItems = items.map((item_id: number) => {
-      return {
-        item_id,
-        point_id,
-      };
+    const pointItems = items
+      .split(',')
+      .map((item: string) => Number(item.trim()))
+      .map((item_id: number) => {
+        return {
+          item_id,
+          point_id,
+        };
     });
   
     await trx('point_items').insert(pointItems);
